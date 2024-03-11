1db755030594ba0f95a7700ec6962f68ef48b91e
MinseoRhie <ummha.dev@gmail.com>
2024-03-12 01:04:48 +0900

    set gitignore

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,13 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <TheWelcome/>
+    </main>
 </template>
+
+<style>
+body {
+    background-color: black;
+}
+</style>
